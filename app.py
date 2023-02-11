ab13bf4b09f870d0208b0f51443efbb1af9b08c5
Ken-yuguchi <JapanUSfriendship@yahoo.co.jp>
2023-02-11 17:44:07 +0900

    2/11 再追加　app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session,redirect
 from datetime import timedelta
 import uuid
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex 
 app.permanent_session_lifetime = timedelta(days=1)
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')        
 
 @app.route('/')
 def index():
