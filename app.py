9e8164da94f024e323952c63a534cf15df7ebfef
Ken-yuguchi <JapanUSfriendship@yahoo.co.jp>
2023-02-13 23:28:17 +0900

    2/13 更新2 app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,session,redirect,request
+from flask import Flask, render_template,session,redirect,request,flash
 from datetime import timedelta
 from models import dbConnect
 from user import User
